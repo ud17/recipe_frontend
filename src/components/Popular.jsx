1a597773f945f5224a062594ac6d1283490e2684
ud17 <upandya1297@gmail.com>
2022-08-31 09:12:28 -0400

    GET: Random Recipes

--- src/components/Popular.jsx
+++ src/components/Popular.jsx
@@ -1,8 +1,31 @@
-import React from 'react'
+import { useState, useEffect } from 'react';
 
 function Popular() {
+
+  const [popular, setPopular] = useState([]);
+
+  useEffect(() => {
+    getPopular();
+  },[]);
+
+  const getPopular = async () => {
+    const api = await fetch(`https://api.spoonacular.com/recipes/random?apiKey=${process.env.REACT_APP_API_KEY}&number=10`);
+    const data = await api.json();
+    setPopular(data.recipes);
+  }
+
   return (
-    <div>Popular</div>
+    <div>
+      {
+        popular?.map((recipe) => {
+          return (
+          <div key={recipe.id}>
+            <p>{recipe.title}</p>
+          </div>
+          )
+        })
+      }
+    </div>
   )
 }
 
