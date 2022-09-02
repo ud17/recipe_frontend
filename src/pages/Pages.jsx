1553c00fcb14f035fc5582b642a13d58fa466385
ud17 <upandya1297@gmail.com>
2022-09-02 07:17:43 -0400

    Search Component&Page added

--- src/pages/Pages.jsx
+++ src/pages/Pages.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import Home from "./Home";
 import Cuisine from "./Cuisine";
+import Searched from "./Searched";
 import { Route, Routes } from "react-router-dom";
 
 function Pages() {
@@ -8,6 +9,7 @@ function Pages() {
     <Routes>
         <Route exact path="/" element={<Home/>}/>
         <Route exact path="/cuisine/:type" element={<Cuisine/>}/>
+        <Route exact path="/searched/:search" element={<Searched />}/>
     </Routes>
   )
 }
